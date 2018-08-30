8cc4bdf6ee714c62047a6eb317ab5334cd3ed96f
makar <makar.pletenetskiy@gmail.com>
2018-08-30 14:19:38 +0300

    load config from IPFS

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import tempfile
@@ -34,7 +35,7 @@ def train(x_train_path, y_train_path, x_test_path, y_test_path, model_path, batc
     logger.info('loss({}):{}, acc({}):{}'.format(loss.__class__.__name__, loss, acc.__class__.__name__, acc))
 
 
-def benchmark_train(working_dir, model_file_path, batch_size, epochs, ethalon_tflops):
+def benchmark_train(working_dir, model_file_path, batch_size, epochs, cost_tflops):
     x_train_path = os.path.join(working_dir, 'x_train.npy')
     y_train_path = os.path.join(working_dir, 'y_train.npy')
     x_test_path = os.path.join(working_dir, 'x_test.npy')
@@ -43,6 +44,7 @@ def benchmark_train(working_dir, model_file_path, batch_size, epochs, ethalon_tf
     metrics = MetricsCollector()
     metrics.start_and_wait_signal()
     metrics.set_pid(os.getpid())
+
     with metrics:
         train(
             x_train_path=x_train_path,
@@ -55,25 +57,12 @@ def benchmark_train(working_dir, model_file_path, batch_size, epochs, ethalon_tf
         )
 
     logger.info('Spent TFLOPs {}'.format(metrics.get_tflops()))
+    logger.info('Spent time: {} s'.format(metrics.total_seconds))
 
-    logger.info('Estimated TFLOP/S is {}'.format(ethalon_tflops / metrics.total_seconds))
-    logger.info('Current TFLOP/S is {}'.format(metrics.get_tflops() / metrics.total_seconds))
+    logger.info('Estimated TFLOP/s is {}'.format(cost_tflops / metrics.total_seconds))
+    logger.info('Current TFLOP/s is {}'.format(metrics.get_tflops() / metrics.total_seconds))
 
-    return ethalon_tflops / metrics.total_seconds
-
-
-def benchmark_ipfs(args):
-    multihash = args.multihash
-    ipfs = IPFS()
-    target_dir = tempfile.mkdtemp()
-    try:
-        time_start = time.time()
-        file_path = ipfs.download(multihash, target_dir)
-        time_end = time.time()
-        file_size = os.path.getsize(file_path)
-        logger.info('IPFS download speed: {} kb/s'.format(file_size/1024/(time_end - time_start)))
-    finally:
-        shutil.rmtree(target_dir)
+    return cost_tflops / metrics.total_seconds
 
 
 class Timer:
@@ -93,22 +82,16 @@ class Timer:
 
 
 def main():
-    epochs = 3
-    batch_size = 128
-
-    torch_cifar_dataset = 'QmRBz8W3K4xDBeu6sDn5kqE4ybndh6CEyFUq1gDEXbYR9V'
-    torch_cifar_model_cnn = 'QmfH9CRJtj66rhQ5jznthamDBHJb37He6Hfy8XmVGG23pw'
-    torch_cifar_model_resnet = 'Qmc7DvhbHtx4oM874dTuiJgYFU5j369gdH1d674TyzBe5n'
-    ethalon_tflops_multihash = 'QmdDs3WEJV77pSgDfpLhM2R1kGkxuukmhLN3UPzaD86xRw'
-
-    model_multihash = torch_cifar_model_resnet
-
     ipfs = IPFS()
+    # read this from IPFS
+    ipfs.api.repo_gc()
+
+    benchmark_config = json.loads(ipfs.read('QmQ1oUUCqRnUMPFTREBtd8ip8xJ8WwAf2ekB9FveCQrSEY'))
     target_dir = tempfile.mkdtemp()
     try:
         timer_model = Timer()
         with timer_model:
-            model_code = ipfs.read(model_multihash)
+            model_code = ipfs.read(benchmark_config['model'])
 
         model_file_path = os.path.join(target_dir, 'model.py')
         with open(model_file_path, 'wb') as model_file:
@@ -116,21 +99,26 @@ def main():
 
         timer_dataset = Timer()
         with timer_dataset:
-            dataset_zip_file_path = ipfs.download(torch_cifar_dataset, target_dir)
+            dataset_zip_file_path = ipfs.download(benchmark_config['dataset'], target_dir)
 
         total_size = os.path.getsize(dataset_zip_file_path) + os.path.getsize(model_file_path)
         download_speed = total_size / (timer_dataset.total_seconds + timer_model.total_seconds)
-        logger.info('IPFS download speed: {} MB/s'.format(download_speed / 1024 / 1024))
 
         zip_ref = zipfile.ZipFile(dataset_zip_file_path, 'r')
         with zip_ref:
             zip_ref.extractall(target_dir)
 
-        ethalon_tflops = int(ipfs.read(ethalon_tflops_multihash))
+        tflops = benchmark_train(
+            working_dir=target_dir,
+            model_file_path=model_file_path,
+            batch_size=benchmark_config['batch_size'],
+            epochs=benchmark_config['epochs'],
+            cost_tflops=benchmark_config['cost_tflops']
+        )
 
-        tflops = benchmark_train(target_dir, model_file_path, batch_size, epochs, ethalon_tflops)
-        logger.info('TFLOPs per seconds: {}'.format(tflops))
-        logger.info('IPFS download speed: {} MB/s'.format(download_speed / 1024 / 1024))
+        logger.info('Result: ipfs speed {} MB/s, performance: {} TFLOP/s'.format(
+            download_speed / 1024 / 1024, tflops))
+
     finally:
         shutil.rmtree(target_dir)
 
