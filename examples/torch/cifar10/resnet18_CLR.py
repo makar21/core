3e1300abd7adf207ab198a041ad2c4ec7e28e4c7
alexeygrankov <alexey.grankov@gmail.com>
2018-09-21 15:44:52 +0300

    Fix CLR for using adjust lr method

--- examples/torch/cifar10/resnet18_CLR.py
+++ examples/torch/cifar10/resnet18_CLR.py
@@ -64,44 +64,5 @@ class Model(model.Model):
         clr_lambda = cyclic_learning_rate(min_lr, max_lr, stepsize)
         self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda=[clr_lambda])
 
-    def train(self, x_path_list: Iterable, y_path_list: Iterable, batch_size: int, current_iteration: int,
-              nb_epochs: int, train_progress: TrainProgress):
-        self.native_model.train()
-        dataset = self.data_preprocessing(x_path_list, y_path_list, transforms=self.transforms_train)
-        batch_size = batch_size * max(1, self._gpu_count)
-        logger.info("Batch size: {}".format(batch_size))
-        loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=False)
-
-        train_history = {'loss': [], 'acc': []}
-        for epoch in range(1, nb_epochs + 1):
-            if self.scheduler is not None:
-                self.scheduler.step()
-            epoch_loss = 0.0
-            # running_loss = 0.0
-            correct = 0
-            for batch_idx, (input_, target) in enumerate(loader, 0):
-                input_, target = input_.to(self.device), target.to(self.device)
-                self.optimizer.zero_grad()
-                output = self.native_model(input_)
-                loss = self._criterion(output, target)
-                epoch_loss += loss.item()
-                _, predicted = torch.max(output.data, 1)
-                correct += predicted.eq(target).sum().item()
-                loss.backward()
-                self.optimizer.step()
-                # running_loss += loss.item()
-
-                # if batch_idx >0 and batch_idx % 200 == 0:
-                #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                #         epoch, batch_idx * len(data), len(loader.dataset),
-                #                100. * batch_idx / len(loader), loss.item()))
-                #     running_loss = 0.0
-            epoch_loss = epoch_loss / len(loader)
-            epoch_acc = correct / len(loader.dataset)
-            logger.info("Epoch #{}: Loss: {:.4f} Acc: {:.2f}".format(epoch, epoch_loss, 100 * epoch_acc))
-            train_history['loss'].append(epoch_loss)
-            train_history['acc'].append(epoch_acc)
-        return train_history
-
     def adjust_learning_rate(self, epoch: int):
-        pass
+        self.scheduler.step(epoch-1)
