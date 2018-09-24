b0d6f4d09df86e3d58b63e44321b7e4f41bfdd3d
27tech <denis@27tech.co>
2018-09-24 20:14:31 +0300

    add resnet50 for cifar

---
+++ examples/torch/cifar10/resnet50.py
@@ -0,0 +1,39 @@
+import torch.optim as optim
+from torch import nn
+from torchvision import transforms
+
+from tatau_core.nn.torch import model
+from tatau_core.nn.torch.models.resnet import ResNet50
+
+
+class Model(model.Model):
+    transform_train = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_eval = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    @classmethod
+    def native_model_factory(cls) -> nn.Module:
+        return ResNet50(num_classes=10)
+
+    def __init__(self):
+        super(Model, self).__init__(
+            optimizer_class=optim.SGD,
+            optimizer_kwargs=dict(lr=0.1, momentum=0.9, weight_decay=1e-4),
+            criterion=nn.CrossEntropyLoss()
+        )
+
+    def adjust_learning_rate(self, epoch: int):
+        # epoch starts from 1, so we could simply check for remainder of the division
+        if epoch % 30 == 0:
+            for param_group in self.optimizer.param_groups:
+                if 'lr' in param_group:
+                    param_group['lr'] = param_group['lr'] * 0.1
