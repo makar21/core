a40ea21c3320bb7742ad6d8e3aac096f7827d169
27tech <denis@27tech.co>
2018-09-24 14:55:20 +0300

    fix transforms

--- examples/torch/cifar10/resnet_fastai.py
+++ examples/torch/cifar10/resnet_fastai.py
@@ -164,7 +164,7 @@ def res_net152():
 
 class Model(model.Model):
 
-    transforms_train = transforms.Compose([
+    transform_train = transforms.Compose([
         transforms.ToPILImage(),
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -172,7 +172,7 @@ class Model(model.Model):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    transforms_eval = transforms.Compose([
+    transform_eval = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
