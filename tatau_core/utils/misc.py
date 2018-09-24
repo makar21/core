0f576d4138449a76ed9f72ab90b7184370b5b635
makar <makar.pletenetskiy@gmail.com>
2018-09-24 18:33:42 +0300

    benchmark: - cifar hardcoded - local benchmark

--- tatau_core/utils/misc.py
+++ tatau_core/utils/misc.py
@@ -1,6 +1,16 @@
 import calendar
 import datetime
+import os
 
 
 def current_timestamp():
     return calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+
+
+def get_dir_size(start_path='.'):
+    total_size = 0
+    for dir_path, dir_names, file_names in os.walk(start_path):
+        for f in file_names:
+            fp = os.path.join(dir_path, f)
+            total_size += os.path.getsize(fp)
+    return total_size
